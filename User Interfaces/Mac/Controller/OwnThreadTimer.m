c9cbaf76fde92dbb21ac9acf0ba05877c9542dd2
Thomas Harte <thomas.harte@gmail.com>
2011-11-22 19:45:41 +0000

    Wrapped runloop runs in an autorelease pool, commented and cleaned up the source generally.

--- User Interfaces/Mac/Controller/OwnThreadTimer.m
+++ User Interfaces/Mac/Controller/OwnThreadTimer.m
@@ -9,7 +9,7 @@
 #import "OwnThreadTimer.h"
 
 @interface CSOwnThreadTimer ()
-@property (assign) BOOL quit;
+@property (assign) BOOL quit;	// i.e. atomic
 @end
 
 @implementation CSOwnThreadTimer
@@ -21,6 +21,7 @@
 	self = [super init];
 	if (self)
 	{
+		// create the timer here since it conveniently records the interval, target, selector and userInfo
 		timer = 
 			[[NSTimer
 				timerWithTimeInterval:interval
@@ -28,7 +29,8 @@
 				selector:selector
 				userInfo:userInfo
 				repeats:YES] retain];
-	
+
+		// start the thread on which we'll run the timer
 		[self start];
 	}
 
@@ -37,23 +39,35 @@
 
 - (void)main
 {
+	// attach the timer to the runloop associated with this thread
 	[[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 
+	// repeat while this own-thead timer hasn't been invalidated
 	while(!self.quit)
 	{
-//		NSLog(@"2s");
+		// create an autorelease pool
+		NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+		// run this thread's runloop for 2 seconds (arbitrarily)
 		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2.0]];
+
+		// drain the pool
+		[pool drain];
 	}
 
-	[timer invalidate];
+	// release the timer
 	[timer release];
 
+	// that's the end for this whole class, so schedule its release
 	[self performSelectorOnMainThread:@selector(autorelease) withObject:nil waitUntilDone:NO];
 }
 
 - (void)invalidate
 {
+	// invalidate the timer immediately so that no further calls out are made
 	[timer performSelector:@selector(invalidate) onThread:self withObject:nil waitUntilDone:YES];
+
+	// request that the thread stop pumping the runloop when next it thinks about it
 	self.quit = YES;
 }
 
