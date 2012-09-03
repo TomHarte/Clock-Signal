ffc0df5fdfae2b3df9944712d04d507bc23d018a
Thomas Harte <thomas.harte@gmail.com>
2012-09-02 20:47:36 -0700

    Added a basic Bresenham run-slice implementation so as correctly to report nanoseconds spent to date without precision issues.

--- Bus/Clock Generator/ClockGenerator.c
+++ Bus/Clock Generator/ClockGenerator.c
@@ -23,7 +23,9 @@ typedef struct
 	unsigned int halfCyclesToDate;
 	CSBusComponent *component;
 	CSBusState currentBusState;
-	uint32_t ticksPerSecond;
+
+	CSComponentNanoseconds timeToNow;
+	int64_t accumulatedError, wholeStep, adjustmentUp, adjustmentDown;
 
 } CSClockGenerator;
 
@@ -43,7 +45,18 @@ void *csClockGenerator_createWithBus(void *bus, uint32_t ticksPerSecond)
 		generator->referenceCountedObject.dealloc = csClockGenerator_destroy;
 		generator->component = csBusNode_createComponent(bus);
 		generator->currentBusState = csBus_defaultState();
-		generator->ticksPerSecond = ticksPerSecond;
+
+		// These constitute a fairly basic implementation of run-slice
+		// Bresenham (since we'll be stepping along the smaller delta and
+		// projecting onto the big)
+		//
+		// i.e. this is like drawing a first quadrant line with
+		// x = 1000000000 (ie, nanoseconds in a second)
+		// y = clock ticks per second
+		generator->wholeStep = 1000000000 / ticksPerSecond;
+		generator->adjustmentUp = (1000000000 % ticksPerSecond) << 1;
+		generator->adjustmentDown = ticksPerSecond << 1;
+		generator->accumulatedError = generator->adjustmentUp - generator->adjustmentDown;
 	}
 
 	return generator;
@@ -58,8 +71,20 @@ void csClockGenerator_runForHalfCycles(void *opaqueGenerator, unsigned int halfC
 	while(halfCycles--)
 	{
 		generator->currentBusState.lineValues ^= CSBusStandardClockLine;
-		generator->component->handlerFunction(generator->component->context, &throwawayState, generator->currentBusState, true, 0);
+		generator->component->handlerFunction(generator->component->context, &throwawayState, generator->currentBusState, true, generator->timeToNow);
 		generator->halfCyclesToDate++;
+
+		// this is standard Bresenham run-slice stuff; add the
+		// whole step, which is floor(y/x), then see whether
+		// doing so has accumulated enough error to push us
+		// up an extra spot
+		generator->timeToNow += generator->wholeStep;
+		generator->accumulatedError += generator->adjustmentUp;
+		if(generator->accumulatedError > 0)
+		{
+			generator->timeToNow++;
+			generator->accumulatedError -= generator->adjustmentDown;
+		}
 	}
 //	printf("clock generator loop done\n");
 }
