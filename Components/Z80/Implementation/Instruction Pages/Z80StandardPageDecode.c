03fc62f488eb8c8dfce059006a7b932ed9f70af9
Thomas Harte <thomas.harte@gmail.com>
2012-09-03 18:36:59 -0700

    Made a couple of implicit truncations explicit and removed use of the GCC case range extension.

--- Components/Z80/Implementation/Instruction Pages/Z80StandardPageDecode.c
+++ Components/Z80/Implementation/Instruction Pages/Z80StandardPageDecode.c
@@ -399,16 +399,17 @@ void llz80_iop_standardPageDecode_imp(LLZ80ProcessorState *z80, LLZ80InternalIns
 			z80->lastSignResult = z80->lastZeroResult =
 			z80->bit5And3Flags = z80->aRegister;
 			
-			int parity = z80->aRegister;
+			uint8_t parity = z80->aRegister;
 			parity ^= (parity >> 4);
 			parity ^= (parity >> 2);
 			parity ^= (parity >> 1);
 
 			z80->generalFlags =
-				newCarry |
-				newHalfCarry |
-				((parity&1) << 3) |
-				(z80->generalFlags&LLZ80FlagSubtraction);
+				(uint8_t)(
+					newCarry |
+					newHalfCarry |
+					((parity&1) << 3) |
+					(z80->generalFlags&LLZ80FlagSubtraction));
 		}
 		break;
 		
@@ -416,9 +417,10 @@ void llz80_iop_standardPageDecode_imp(LLZ80ProcessorState *z80, LLZ80InternalIns
 		{
 			z80->bit5And3Flags = z80->aRegister;
 			z80->generalFlags =
-				(z80->generalFlags & LLZ80FlagParityOverflow) |
-				((z80->generalFlags & LLZ80FlagCarry) << 4) |	// so half carry is what carry was
-				((z80->generalFlags^LLZ80FlagCarry)&LLZ80FlagCarry);
+				(uint8_t)(
+					(z80->generalFlags & LLZ80FlagParityOverflow) |
+					((z80->generalFlags & LLZ80FlagCarry) << 4) |	// so half carry is what carry was
+					((z80->generalFlags^LLZ80FlagCarry)&LLZ80FlagCarry));
 				// implicitly setting subtract and half carry to 0
 		}
 		break;
@@ -643,7 +645,15 @@ void llz80_iop_standardPageDecode_imp(LLZ80ProcessorState *z80, LLZ80InternalIns
 		}
 		break;
 
-		case 0x40 ... 0x7f:	// ld r, r
+		// ld r, r
+		case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47:
+		case 0x48: case 0x49: case 0x4a: case 0x4b: case 0x4c: case 0x4d: case 0x4e: case 0x4f:
+		case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57:
+		case 0x58: case 0x59: case 0x5a: case 0x5b: case 0x5c: case 0x5d: case 0x5e: case 0x5f:
+		case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67:
+		case 0x68: case 0x69: case 0x6a: case 0x6b: case 0x6c: case 0x6d: case 0x6e: case 0x6f:
+		case 0x70: case 0x71: case 0x72: case 0x73: case 0x74: case 0x75: case 0x76: case 0x77:
+		case 0x78: case 0x79: case 0x7a: case 0x7b: case 0x7c: case 0x7d: case 0x7e: case 0x7f:
 		{
 			unsigned char *source = rTable[opcode&7];
 			unsigned char *destination = rTable[(opcode >> 3)&7];
@@ -720,7 +730,15 @@ void llz80_iop_standardPageDecode_imp(LLZ80ProcessorState *z80, LLZ80InternalIns
 		}
 		break;
 
-		case 0x80 ... 0xbf: // [most of] the ALU operations, en masse
+		// [most of] the ALU operations, en masse
+		case 0x80: case 0x81: case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87:
+		case 0x88: case 0x89: case 0x8a: case 0x8b: case 0x8c: case 0x8d: case 0x8e: case 0x8f:
+		case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97:
+		case 0x98: case 0x99: case 0x9a: case 0x9b: case 0x9c: case 0x9d: case 0x9e: case 0x9f:
+		case 0xa0: case 0xa1: case 0xa2: case 0xa3: case 0xa4: case 0xa5: case 0xa6: case 0xa7:
+		case 0xa8: case 0xa9: case 0xaa: case 0xab: case 0xac: case 0xad: case 0xae: case 0xaf:
+		case 0xb0: case 0xb1: case 0xb2: case 0xb3: case 0xb4: case 0xb5: case 0xb6: case 0xb7:
+		case 0xb8: case 0xb9: case 0xba: case 0xbb: case 0xbc: case 0xbd: case 0xbe: case 0xbf:
 		{
 			unsigned char *source = rTable[opcode&7];
 			int operation = (opcode >> 3)&7;
